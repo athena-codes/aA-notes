3fe8f55a91a310045db57d190a88cb4257859f72
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2022-11-06 21:42:12 -0500

    week 11 graph path notes

--- MOD2/week-12/graph-paths.js
+++ MOD2/week-12/graph-paths.js
@@ -54,5 +54,62 @@ console.log(hasPathBreadthFirst(graph, 'k', 'h')) // -> false, since k and h are
 
 
 
+// Undirected Path
+
+const edges = [
+   ['i', 'j'],
+   ['k', 'i'],
+   ['m', 'k'],
+   ['k', 'l'],
+   ['o', 'n']
+]
 
 
+const undirectedPath = (edges, nodeA, nodeB) => {
+  const graph = buildGraph(edges)
+
+  return hasPath(graph, nodeA, nodeB, new Set())
+}
+
+
+const hasPath = (graph, src, dst, visited) => {
+  // base case
+  if (src === dst) return true
+  // use set to account for cycles + infinite loop
+  if (visited.has(src)) return false
+  // if not false, then add to set
+  visited.add(src)
+  // traverse neighbors of the graph from source
+  for (let neighbor of graph[src]) {
+    // if our neighbor is connected to dst, then we know a path from src to dst exists
+    // since src is next to neighbor
+    if (hasPath(graph, neighbor, dst, visited) === true) {
+      return true
+    }
+  }
+  return false
+}
+
+
+const buildGraph = (edges) => {
+  const graph = {}
+
+  for (let edge of edges) {
+    const [a, b] = edge
+
+    if (!(a in graph)) graph[a] = []
+    if (!(b in graph)) graph[b] = []
+     graph[a].push(b)     // undirected graph, so adjacency list must be symmetric
+     graph[b].push(a)    // if a is in b's neighbors, b should be in a's neighbors
+  }
+  return graph
+}
+
+console.log(undirectedPath(edges, 'j', 'm'))  // -> true
+
+
+
+const connectedComponentsCount = (graph) => {
+  
+
+}
