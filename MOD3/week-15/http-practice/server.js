30aa43750917aa32c3ae73380c7b38d841a9b11a
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2022-11-30 17:19:58 -0500

    added notes about creating server + req/res

--- MOD3/week-15/http-practice/server.js
+++ MOD3/week-15/http-practice/server.js
@@ -8,21 +8,41 @@ const port = 3000
 // use createServer method to create a new server
 const server = http.createServer((req, res) => {
 //   console.log(req)
-console.log(res)
+// console.log(res)
+  res.status = 200
   res.writeHead(200, { 'Content-Type': 'text/html' })
   fs.readFile('index.html', function(error, data) {
-    if (error) {
-        res.writeHead(404)
-        res.write('Error: File Not Found')
-    } else {
-        res.write(data)
-    }
-    res.end()
+      if (error) {
+          res.writeHead(404)
+          res.write('Error: File Not Found')
+        } else {
+            res.write(data)
+        }
+        res.end('The end!')
   })
 })
 
+const server2 = http.createServer((req, res) => {
+if (req.method === 'GET' && req.url === '/') {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  return res.end('Splash Page')
+}
+if (req.method === 'POST' && req.url === '/cat') {
+  res.statusCode = 201
+  res.setHeader('Content-Type', 'text/plain')
+  return res.end('Created a Cat!')
+}
+if (req.method === 'POST' && req.url === '/helloworld') {
+  res.statusCode = 201
+  res.setHeader('Content-Type', 'text/plain')
+  return res.end('Hello World!')
+}
 
-server.listen(port, function(error) {
+})
+
+
+server2.listen(port, function(error) {
     if (error) {
       console.log('Something went wrong', error)
     } else {
--- MOD3/week-15/parsing-requests/server.js
+++ MOD3/week-15/parsing-requests/server.js
@@ -1,11 +1,25 @@
 const http = require('http')
 const fs = require('fs')
-const port = 5000
-
+const port = 8000
 
 const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/css')
+  res.write('Hello')
+  res.write(' ')
+  res.write('World')
+  res.write('!')
+
   let reqBody = ''
   req.on('data', data => {
     reqBody += data
   })
+
+  req.on('end', () => {
+    console.log(reqBody)
+  })
+})
+
+server.listen(port, () => {
+  console.log('Server is listening on port ' + port)
 })
