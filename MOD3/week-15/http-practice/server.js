cf37253b17c7f4525d1e52d78e45e1d23577c3b1
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2022-11-30 16:56:10 -0500

    practice running a local host server + PM requests

--- MOD3/week-15/http-practice/server.js
+++ MOD3/week-15/http-practice/server.js
@@ -2,12 +2,30 @@
 
 // inmport http pckg
 const http = require('http')
+const fs = require('fs')
+const port = 3000
 
 // use createServer method to create a new server
 const server = http.createServer((req, res) => {
-// ...
+//   console.log(req)
+console.log(res)
+  res.writeHead(200, { 'Content-Type': 'text/html' })
+  fs.readFile('index.html', function(error, data) {
+    if (error) {
+        res.writeHead(404)
+        res.write('Error: File Not Found')
+    } else {
+        res.write(data)
+    }
+    res.end()
+  })
 })
 
-const port = 5000
 
-server.listen(port, () => console.log('Server is listening on port', port))
+server.listen(port, function(error) {
+    if (error) {
+      console.log('Something went wrong', error)
+    } else {
+      console.log('Server is listening on port ' + port)
+    }
+})
