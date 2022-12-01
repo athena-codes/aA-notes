5707a9a72adec1381e00611cfec382bee9f631b9
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2022-12-01 00:57:17 -0500

    created server that sends cute cat picture, cool

--- MOD3/week-15/http-practice/server.js
+++ MOD3/week-15/http-practice/server.js
@@ -6,43 +6,55 @@ const fs = require('fs')
 const port = 3000
 
 // use createServer method to create a new server
-const server = http.createServer((req, res) => {
-//   console.log(req)
-// console.log(res)
-  res.status = 200
-  res.writeHead(200, { 'Content-Type': 'text/html' })
-  fs.readFile('index.html', function(error, data) {
-      if (error) {
-          res.writeHead(404)
-          res.write('Error: File Not Found')
-        } else {
-            res.write(data)
-        }
-        res.end('The end!')
-  })
-})
+// const server = http.createServer((req, res) => {
+// //   console.log(req)
+// // console.log(res)
+//   res.status = 200
+//   res.writeHead(200, { 'Content-Type': 'text/html' })
+//   fs.readFile('index.html', function(error, data) {
+//       if (error) {
+//           res.writeHead(404)
+//           res.write('Error: File Not Found')
+//         } else {
+//             res.write(data)
+//         }
+//         res.end('The end!')
+//   })
+// })
 
-const server2 = http.createServer((req, res) => {
-if (req.method === 'GET' && req.url === '/') {
+// example of sending messages thru server
+
+// const server2 = http.createServer((req, res) => {
+// if (req.method === 'GET' && req.url === '/') {
+//   res.statusCode = 200
+//   res.setHeader('Content-Type', 'text/plain')
+//   return res.end('Splash Page')
+// }
+// if (req.method === 'POST' && req.url === '/cat') {
+//   res.statusCode = 201
+//   res.setHeader('Content-Type', 'text/plain')
+//   return res.end('Created a Cat!')
+// }
+// if (req.method === 'POST' && req.url === '/helloworld') {
+//   res.statusCode = 201
+//   res.setHeader('Content-Type', 'text/plain')
+//   return res.end('Hello World!')
+// }
+
+// })
+
+// sends an image over the server as response
+
+const server3 = http.createServer((req, res) => {
+  const catImage = fs.readFileSync('./images/cat.png')
+
   res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  return res.end('Splash Page')
-}
-if (req.method === 'POST' && req.url === '/cat') {
-  res.statusCode = 201
-  res.setHeader('Content-Type', 'text/plain')
-  return res.end('Created a Cat!')
-}
-if (req.method === 'POST' && req.url === '/helloworld') {
-  res.statusCode = 201
-  res.setHeader('Content-Type', 'text/plain')
-  return res.end('Hello World!')
-}
-
+  res.setHeader('Content-Type', 'image/png')
+  res.end(catImage)
 })
 
 
-server2.listen(port, function(error) {
+server3.listen(port, function(error) {
     if (error) {
       console.log('Something went wrong', error)
     } else {
