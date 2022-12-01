e4ba6fbfc2878084cfc1d662d9a91435cecb1fbe
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2022-12-01 03:59:32 -0500

    working on register login app

---
+++ MOD3/week-15/passport-login/server.js
@@ -0,0 +1,65 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+const flash = require('express-flash')
+const session = require('express-session')
+const passport = require('passport')
+
+
+const initializePassport = require('./views/passport-config')
+initializePassport(passport, email => {
+     users.find(user => user.email === email)
+})
+
+const users = []
+
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session( {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.get('/', (req, res) => {
+  res.render('index.ejs', { name: 'Athena' })
+})
+
+app.get('/login', (req, res) => {
+  res.render('login.ejs')
+})
+
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: 'login',
+    failureFlash: true
+}))
+
+app.get('/register', (req, res) => {
+  res.render('register.ejs')
+})
+
+app.post('/register', async (req, res) => {
+    try {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10)
+      users.push({
+        id: Date.now().toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+      })
+       res.redirect('/login')
+     } catch {
+        res.redirect('/register')
+    }
+  console.log(users)
+})
+
+app.listen(3000)
--- MOD3/week-15/template-engines/app.js
+++ MOD3/week-15/template-engines/app.js
@@ -3,8 +3,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set("view engine", "pug")
+
+
 app.get("/", function(req, res) {
-    res.send("Hello World!")
+  res.render('index', { title: 'Hello', message: 'Hello World!' })
 })
 
 app.listen(port, () => console.log(`Template engine listening on ${port}`))
