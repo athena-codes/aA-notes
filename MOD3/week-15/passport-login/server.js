a27d3572b8c9c030b3688acdf33f38ee3b82afc7
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2022-12-01 16:23:55 -0500

    added notes

--- MOD3/week-15/passport-login/server.js
+++ MOD3/week-15/passport-login/server.js
@@ -20,6 +20,7 @@ initializePassport(
 const users = []
 
 app.set('view-engine', 'ejs')
+// take forms from email and pw and access them inside req var in post method
 app.use(express.urlencoded({ extended: false }))
 app.use(flash())
 app.use(
@@ -33,14 +34,19 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(methodOverride('_method'))
 
+// page redirected to when user logs in (homepage) + shows name based on user input
 app.get('/', checkAuthenticated, (req, res) => {
+                          // shows name that user inputs
   res.render('index.ejs', { name: req.user.name })
 })
 
+// page redirected to log in page
 app.get('/login', checkNotAuthenticated, (req, res) => {
   res.render('login.ejs')
 })
 
+// when login info is sent, redirect to homepage if credentials match
+// or if wrong password redirect to login screen
 app.post(
   '/login',
   checkNotAuthenticated,
@@ -55,9 +61,11 @@ app.get('/register', checkNotAuthenticated, (req, res) => {
   res.render('register.ejs')
 })
 
+// hashes password on each user login
 app.post('/register', checkNotAuthenticated, async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    // add users to user array w/ all info inputed (password hashed)
     users.push({
       id: Date.now().toString(),
       name: req.body.name,
@@ -66,14 +74,21 @@ app.post('/register', checkNotAuthenticated, async (req, res) => {
     })
     res.redirect('/login')
   } catch {
+    // if failure redirect to register page
     res.redirect('/register')
   }
+  console.log(users)
 })
 
-app.delete('/logout', (req, res) => {
-  req.logOut()
-  res.redirect('/login')
+app.delete('/logout', (req, res, next) => {
+  req.logOut(err => {
+    if (err) {
+      return next(err)
+    }
+    res.redirect('/login')
+  })
 })
+
 
 function checkAuthenticated (req, res, next) {
   if (req.isAuthenticated()) {
@@ -90,4 +105,5 @@ function checkNotAuthenticated (req, res, next) {
   next()
 }
 
+// app running on localhost:3000
 app.listen(3000)
