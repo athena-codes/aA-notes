04b80570e8b64d0f3e379a29d94f51f6d9f445da
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2022-12-07 17:07:45 -0500

    countries and flag html AJAX practice

---
+++ MOD3/week-16/ajax/index.js
@@ -0,0 +1,26 @@
+function showCountries() {
+    let xhr = new XMLHttpRequest()
+
+    xhr.open('GET', 'https://restcountries.com/v3.1/all', true)
+
+    xhr.onload = function() {
+      if(xhr.status == 200) {
+        console.log('Success')
+        let countries = JSON.parse(this.response)
+        console.log(countries)
+        countries.forEach(country => {
+           const countryCard = document.createElement('div')
+           countryNameKey = country.name
+           countryName = countryNameKey.common
+           countryCard.innerHTML = countryName
+           document.getElementById('feed').appendChild(countryCard)
+           const countryCardImage = document.createElement('img')
+           countryFlag = country.flags
+           countryCardImage.src = countryFlag.png
+           countryCard.appendChild(countryCardImage)
+
+        })
+      }
+    }
+    xhr.send()
+}
