b311c8fb28965acd075d702818289100cf3d6c0e
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2022-12-14 18:22:43 -0500

    added header inner styling JS

--- MOD3/week-17/rock-paper-scissors/main.mjs
+++ MOD3/week-17/rock-paper-scissors/main.mjs
@@ -1,22 +1,40 @@
 // When DOM loads, prints msg to console
 
-window.onload = e => {
-  console.log('Window has loaded')
-}
+// window.onload = e => {
+//   console.log('Window has loaded')
+// }
 
 
 // Better way to run program when DOM is loaded
 
 window.addEventListener('DOMContentLoaded', async () => {
-    // console.log('Window is loaded')
+      // console.log('Window is loaded')
+    let body = document.getElementById('body')
+
+    // inline javascript styling -> adds to HTML in DOM
+    body.style.backgroundColor = 'white'
+
+    // returns HTML collection
+    let h2sHTML = document.getElementsByTagName('h2')
+    let h2sNode = document.querySelectorAll('.header')
+    console.log(h2sHTML)
+    console.log(h2sNode)
+
+    // console.log(h2sNode.forEach((ele) => {
+    //     // console.log(ele)
+    // }))
+    let playerName = document.getElementById('player-name')
+    playerName.innerHTML = '<p> Player </p>'
+
+    let playerScore = document.getElementById('player-score')
+    playerScore.innerText = 0
+
+    let cpuName = document.getElementById('cpu-name')
+    cpuName.innerText = 'CPU'
+
+    let cpuScore = document.getElementById('cpur-score')
+    cpuScore.innerText = 0
 
 
 
-
-
-
-
-
-
-
-})
+  })
