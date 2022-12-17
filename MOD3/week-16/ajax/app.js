80c29f2e28cc1dbdc42eb016f1d74524a29327cd
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2022-12-17 13:53:30 -0500

    added notes on event listeners

--- MOD3/week-16/ajax/app.js
+++ MOD3/week-16/ajax/app.js
@@ -1,26 +1,26 @@
 // USING FETCH API
 
-// const flag = document.createElement('img')
-// document.body.appendChild(flag)
+const flag = document.createElement('img')
+document.body.appendChild(flag)
 
-// console.log("Let's go!")
+console.log("Let's go!")
 
-// fetch('https://restcountries.com/v3.1/all')
-//   .then((res) => {
-//      return res.json()
-//   }).then((json) => {
-//     flag.src = json[0].flag
-//     console.log(flag)
-//     console.log("Flag added!")
-//   }).catch((err) => {
-//     console.log('Errors:' + err.message)
-//   })
+fetch('https://restcountries.com/v3.1/all')
+  .then((res) => {
+     return res.json()
+  }).then((json) => {
+    flag.src = json[0].flag
+    console.log(flag)
+    console.log("Flag added!")
+  }).catch((err) => {
+    console.log('Errors:' + err.message)
+  })
 
-//   console.log("Done!")
+  console.log("Done!")
 
 // --response represents completion or failure of an HTTP request
-// let response = fetch('https://restcountries.com/v3.1/all')
-// console.log(response)
+let response = fetch('https://restcountries.com/v3.1/all')
+console.log(response)
 
 
 // lets go and done print to console, however button is blocked from printing until click event occurs
