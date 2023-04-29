47e4373276705305d1170e1b27eb4cbda8f7c641
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-04-29 09:49:07 -0400

    added notes on dunder methods

--- MOD6/week-34/day-4/classes.py
+++ MOD6/week-34/day-4/classes.py
@@ -1,3 +1,8 @@
+# DUNDER METHODS:
+# Function names that start and end with two underscores are special built-in functions that
+# Python uses in certain ways. The __init__ class method is a dunder method
+
+
 # Angry Bird class example:
 class AngryBird:
     def __init__(self):
@@ -7,6 +12,13 @@ class AngryBird:
         self.x = 0
         self.y = 0
 
-# to create a new instance of a class in python, invoke it like a function 
+    def move_up_by(self, delta):
+        self.y += delta
+
+# to create a new instance of a class in python, invoke it like a function
 bird = AngryBird()
 print(bird.x, bird.y)  # > 0 0
+
+# do not need to pass in 'self' value, Python does this automatically
+bird.move_up_by(8)
+print(bird.x, bird.y)  #> 0 8
