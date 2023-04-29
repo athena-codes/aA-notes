d646ae2408e56346afffdbfbec10a304ddd1bfa3
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-04-29 09:54:00 -0400

    added notes on private variables

--- MOD6/week-34/day-4/classes.py
+++ MOD6/week-34/day-4/classes.py
@@ -22,3 +22,41 @@ print(bird.x, bird.y)  # > 0 0
 # do not need to pass in 'self' value, Python does this automatically
 bird.move_up_by(8)
 print(bird.x, bird.y)  #> 0 8
+
+# setting the initial state of x & y as 0
+class AngryBird2:
+    def __init__(self, x=0, y=0):
+        """
+        Construct a new AngryBird2 by setting its position to (0, 0).
+        """
+        self.x = x
+        self.y = y
+
+    def move_up_by(self, delta):
+        self.y += delta
+
+
+b1 = AngryBird2()
+b2 = AngryBird2(x=1)
+b3 = AngryBird2(y=18)
+b4 = AngryBird2(10, 10)
+
+print('Bird 1', b1.x, b1.y)
+print('Bird 2', b2.x, b2.y)
+print('Bird 3', b3.x, b3.y)
+print('Bird 4', b4.x, b4.y)
+
+#  OUTPUT:
+# Bird 1 0 0
+# Bird 2 1 0
+# Bird 3 0 18
+# Bird 4 10 10
+
+#  PRIVATE VARIABLES:
+# If you don't want others to interact directly with the instance variables in your class,
+# you should put a leading underscore on them. Since the AngryBird class is in charge of its
+# x and y instance variables, that means the names of those variables should have a leading underscore.
+
+# EXAMPLE:
+# self._x = x
+# self._y = y
