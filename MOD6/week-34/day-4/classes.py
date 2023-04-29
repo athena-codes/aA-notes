f2e5f37ea60f1e2702b5000bf577f0a70a08f6ec
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-04-29 09:45:53 -0400

    more classes notes

--- MOD6/week-34/day-4/classes.py
+++ MOD6/week-34/day-4/classes.py
@@ -2,7 +2,11 @@
 class AngryBird:
     def __init__(self):
         """
-        Construct a new AngryBird by setting its position to (0, 0).
+        Construct a new Angry Bird by setting its position to (0, 0).
         """
         self.x = 0
         self.y = 0
+
+# to create a new instance of a class in python, invoke it like a function 
+bird = AngryBird()
+print(bird.x, bird.y)  # > 0 0
