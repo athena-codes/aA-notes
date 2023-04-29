25ef81489904598b7077c4b442ae467979362435
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-04-29 09:41:13 -0400

    added notes on python classes

---
+++ MOD6/week-34/day-4/classes.py
@@ -0,0 +1,8 @@
+# Angry Bird class example:
+class AngryBird:
+    def __init__(self):
+        """
+        Construct a new AngryBird by setting its position to (0, 0).
+        """
+        self.x = 0
+        self.y = 0
