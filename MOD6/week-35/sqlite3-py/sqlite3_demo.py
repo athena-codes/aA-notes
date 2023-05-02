f8ed9db92b12b1eaed347dc0fc5fbe5f2dfcb39a
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-05-02 17:27:47 -0400

    created sqlite3 database

--- MOD6/week-35/sqlite3-py/sqlite3_demo.py
+++ MOD6/week-35/sqlite3-py/sqlite3_demo.py
@@ -1,4 +1,36 @@
 ''' SQLITE 3
 * a built-in package is used to connect to SQLite3 in Python
+=============================================================
+                    DB SCHEMA
+=============================================================
+                    
+CREATE TABLE owners (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL
+);
 
+-- Make and model should have their own tables
+-- Simplified for now
+CREATE TABLE cars (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  manu_year INTEGER NOT NULL,
+  make VARCHAR(255),
+  model VARCHAR(255),
+  owner_id INTEGER NOT NULL,
+  FOREIGN KEY (owner_id) REFERENCES owners(id)
+);
+
+INSERT INTO owners (first_name, last_name, email)
+VALUES
+('Tim', 'Petrol', 'rotary@fast.com'),
+('Ryan', 'Runner', '10sec@jdm.com'),
+('Tia', 'Petrol', 'typer@wtec.com');
+
+INSERT INTO cars (manu_year, make, model, owner_id)
+VALUES
+(1993, 'Mazda', 'Rx7', 1),
+(1995, 'Mitsubishi', 'Eclipse', 2),
+(1994, 'Acura', 'Integra', 3);
 '''
