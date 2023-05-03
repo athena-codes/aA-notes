dfe2b4e1ce267b687047084293abe055686e90c0
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-05-03 18:59:08 -0400

    added notes on setting up routes

---
+++ MOD6/flask/server.py
@@ -0,0 +1,13 @@
+# Basic flask application setup
+
+from flask import Flask
+
+app = Flask(__name__)
+
+# The path for the route is the parameter passed to the app.route decorator
+@app.route('/')
+def hello():
+    return '<h1>Hello, world!</h1>'
+
+# Start application:
+# pipenv run flask run
---
+++ MOD6/week-35/flask-intro/main.py
@@ -0,0 +1,17 @@
+# Basic flask application setup
+
+# Start by importing flask package:
+
+from flask import Flask
+
+app = Flask(__name__)
+
+# The path for the route is the parameter passed to the app.route decorator
+
+
+@app.route('/')
+def hello():
+    return '<h1>Hello, world!</h1>'
+
+# Start application:
+# pipenv run flask run
