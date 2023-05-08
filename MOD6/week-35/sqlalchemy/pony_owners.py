4452838d8c6844043876dd541859916a0c2c9264
Athena Chiarello <107880032+athenabean@users.noreply.github.com>
2023-05-08 17:09:11 -0400

    added more notes on sql alchemy

--- MOD6/week-35/sqlalchemy/pony_owners.py
+++ MOD6/week-35/sqlalchemy/pony_owners.py
@@ -1,3 +1,15 @@
 from sqlalchemy import create_engine
 
-engine = create_engine("sqlite:///dev.db")
+db_url = "sqlite:///dev.db"
+engine = create_engine(db_url)
+
+with engine.connect() as connection:
+    result = connection.execute("""
+        SELECT o.first_name, o.last_name, p.name
+        FROM owners o
+        JOIN ponies p ON (o.id = p.owner_id)
+    """)
+    for row in result:
+        print(row["first_name"], row["last_name"], "owns", row["name"])
+
+engine.dispose()
